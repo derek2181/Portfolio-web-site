6d86f6093d64266f5f820a2d3fd07af79cee2838
derek2181 <dereka40@hotmail.com>
2022-11-11 02:43:20 -0600

    About me section and mobile apps

--- js/carousel-initializer.js
+++ js/carousel-initializer.js
@@ -1,8 +1,44 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
+    var firstCarousel= $(".technologies-owl");
+    var secondCarousel=$(".projects-web");
+    var thirdCarousel=$(".projects-mobile");
+    firstCarousel.owlCarousel({
         loop:true,
-        margin:10,
-        autoplaySpeed:1000,
+        autoplay:true,
+        autoplaySpeed:2000,
+        autoplayTimeout : 7400,
+        infiniteLoop:true,
+        touchDrag:true,
+        mouseDrag:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:1,
+                nav:false,
+                loop:true,
+     
+            },
+            1000:{
+                items:1,
+                nav:false,
+                loop:true
+            }
+        }
+    });
+
+
+
+    secondCarousel.owlCarousel({
+        loop:true,
+        autoplay:true,
+       
+        autoplaySpeed:1500,
+        autoplayTimeout : 10000,
+        infiniteLoop:true,
         touchDrag:true,
         mouseDrag:true,
         responsiveClass:true,
@@ -18,7 +54,35 @@ $(document).ready(function(){
             1000:{
                 items:1,
                 nav:false,
-                loop:true
+                loop:true,
+                dots:true
+            }
+        }
+    });
+    thirdCarousel.owlCarousel({
+        loop:true,
+        autoplay:true,
+        autoHeight:true,
+        autoplaySpeed:1500,
+        autoplayTimeout : 10000,
+        infiniteLoop:true,
+        touchDrag:true,
+        mouseDrag:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:1,
+                nav:false
+            },
+            1000:{
+                items:1,
+                nav:false,
+                loop:true,
+                dots:true
             }
         }
     });
